343115831c78f0115b6c6c874630f556249349bb
Michaela Bajanova <xbajanov@fi.muni.cz>
2019-08-11 21:25:19 +0200

    User to FC

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Header.scss';
+import User from "./User";
 
 class Header extends React.Component {
 
@@ -9,20 +10,6 @@ class Header extends React.Component {
                 <User/>
             </header>
         )
-    }
-}
-
-class User extends React.Component {
-
-    render = () => {
-        return (
-            <div className="user">
-                <div className="user__photo-wrapper">
-                    <img className="user__photo" src={require('../images/puppy.jpg')} alt="User"/>
-                </div>
-                <div className="user__name">John Doe</div>
-            </div>
-        );
     }
 }
 
---
+++ src/components/User.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import '../styles/User.scss';
+
+const User: React.FunctionComponent = () => {
+
+    return (
+        <div className="user">
+            <div className="user__photo-wrapper">
+                <img className="user__photo" src={require('../images/puppy.jpg')} alt="User"/>
+            </div>
+            <div className="user__name">John Doe</div>
+        </div>
+    );
+};
+
+export default User;
