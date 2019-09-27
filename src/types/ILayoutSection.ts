db6064f4c9f8edc5fa6a105976cbf92773f3be08
Michaela Bajanova <xbajanov@fi.muni.cz>
2019-09-27 13:06:48 +0200

    described section layout with interface

---
+++ src/types/ILayoutSection.ts
@@ -0,0 +1,9 @@
+// x, y are coordinates of position in the grid
+// start and end describe width of section
+export interface ILayoutSection {
+    id: number,
+    x: number,
+    y: number,
+    start: number,
+    end: number,
+}
